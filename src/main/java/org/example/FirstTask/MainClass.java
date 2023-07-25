e786b09c765de438bb80fe7c2f21f4107ecabf7d
Lev <lev.lvi2@gmail.com>
2023-07-25 17:33:55 +0300

    .

--- src/main/java/org/example/FirstTask/MainClass.java
+++ src/main/java/org/example/FirstTask/MainClass.java
@@ -9,9 +9,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-import com.google.gson.reflect.TypeToken;
-
-import java.util.Map;
 
 
 public class MainClass {
